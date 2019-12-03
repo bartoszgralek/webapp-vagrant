757836c0cfde44366743e6588e0b723c0248615a
Kuba Stepaniak <kuba.stepaniak@gmail.com>
2019-12-03 22:02:46 +0100

    Services added

---
+++ backend/start-backend.sh
@@ -0,0 +1,3 @@
+cd /home/kuba/Documents/repos/uczelnia/webapp-vagrant/backend
+sudo ./mvnw clean install
+sudo ./mvnw spring-boot:run &
---
+++ frontend/start-frontend.sh
@@ -0,0 +1,2 @@
+cd /home/kuba/Documents/repos/uczelnia/webapp-vagrant/frontend
+ng serve --host 0.0.0.0 --port=4500
