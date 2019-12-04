29104057030a425681463ebf90b0303944eb9ba6
Kuba Stepaniak <kuba.stepaniak@gmail.com>
2019-12-04 23:11:52 +0100

    Dockerfile for java added. Start-docker.sh added.

--- start-docker.sh
+++ start-docker.sh
@@ -1,2 +1,5 @@
+docker build -t backend-image ./backend/
+docker run --name backend-app -d -p 8081:8080 backend-image 
+
 docker build -t frontend-image ./frontend/
 docker run --name frontend-app -d -p 8080:80 frontend-image 
